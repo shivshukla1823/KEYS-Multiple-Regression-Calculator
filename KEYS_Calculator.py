cf05a528c39e8fa48b24d1bf8588e62b0110dd6d
shivshukla1823 <135557737+shivshukla1823@users.noreply.github.com>
2025-08-09 09:26:57 -0700

    Update KEYS_Calculator.py

--- KEYS_Calculator.py
+++ KEYS_Calculator.py
@@ -9,9 +9,9 @@ nr = st.number_input("Researchers", min_value=0, step=1)
 np = st.number_input("Drug Developers", min_value=0, step=1)
 
 # Coefficients
-hmod = -49.74
-rmod = -89.90
-pmod = -35.31
+hmod = 49.74
+rmod = 89.90
+pmod = 35.31
 
 # Calculations
 daly_h = hmod * nh
@@ -24,6 +24,7 @@ st.write(f"Physicians: {daly_h:,.2f}")
 st.write(f"Researchers: {daly_r:,.2f}")
 st.write(f"Drug Developers: {daly_p:,.2f}")
 st.subheader(f"**Total: {total:,.2f} DALYs avoided**")
+
 
 
 
